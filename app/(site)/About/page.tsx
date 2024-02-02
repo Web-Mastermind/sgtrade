52432236eeacfb8c6d20e3987f0476b970273f53
Web-Mastermind <AZEAgent@gmail.com>
2024-02-02 15:51:22 +0400

    added fragment

--- app/(site)/About/page.tsx
+++ app/(site)/About/page.tsx
@@ -3,15 +3,17 @@ import styles from "./style.module.css"
 
 const About = () => {
   return (
-    <main className={styles.aboutMain}>
-      <div className={styles.aboutItems}>
-        <div className="container">
-          <h2 className={`${styles.aboutTitle} ${styles.white}`}>Haqqımızda</h2>
-          <Image className={styles.aboutImage} src="/assets/icon/solid.png" alt="Border Solid" width={88} height={2} />
-          <p className={`${styles.aboutText} ${styles.width} ${styles.color} ${styles.white}`}>2021-ci ildə Şərqi Avropa içki sektorunda özəl etiket istehsal xətti yaratmaq məqsədi ilə qurulan şirkətimiz 2022-ci ildə baş verən qlobal hadisələrlə əlaqədar uğursuzluqlar yaşadı. Bununla belə, 2023-cü ilin yanvarında biz hərtərəfli yenidən qiymətləndirmədən keçdik və dəyişən şəraitə uğurla uyğunlaşdıq.</p>
+    <>
+      <main className={styles.aboutMain}>
+        <div className={styles.aboutItems}>
+          <div className="container">
+            <h2 className={`${styles.aboutTitle} ${styles.white}`}>Haqqımızda</h2>
+            <Image className={styles.aboutImage} src="/assets/icon/solid.png" alt="Border Solid" width={88} height={2} />
+            <p className={`${styles.aboutText} ${styles.width} ${styles.color} ${styles.white}`}>2021-ci ildə Şərqi Avropa içki sektorunda özəl etiket istehsal xətti yaratmaq məqsədi ilə qurulan şirkətimiz 2022-ci ildə baş verən qlobal hadisələrlə əlaqədar uğursuzluqlar yaşadı. Bununla belə, 2023-cü ilin yanvarında biz hərtərəfli yenidən qiymətləndirmədən keçdik və dəyişən şəraitə uğurla uyğunlaşdıq.</p>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
 
