2f3db772807982dc9cc5733a2d571d5b496945d7
Web-Mastermind <AZEAgent@gmail.com>
2024-02-02 22:28:22 +0400

    added links for news page

--- src/links/Links.ts
+++ src/links/Links.ts
@@ -3,7 +3,7 @@ export const NavLinks = [
   { id: 2, to: "mailto:info@sgtrade.lt", title: "info@sgtrade.lt", imageSrc: "/assets/icon/mail.svg", alt: "Phone Icon", width: 30, height: 30 },
   { id: 3, to: "/impt", title: "Import/Export", imageSrc: "/assets/icon/import.svg", alt: "Phone Icon", width: 15, height: 15 },
   { id: 4, to: "/About", title: "About Us", imageSrc: "/assets/icon/about.svg", alt: "Phone Icon", width: 15, height: 15 },
-  { id: 5, to: "/news", title: "News", imageSrc: "/assets/icon/news.svg", alt: "Phone Icon", width: 15, height: 15 },
+  { id: 5, to: "/News", title: "News", imageSrc: "/assets/icon/news.svg", alt: "Phone Icon", width: 15, height: 15 },
   { id: 6, to: "/contacts", title: "Contacts", imageSrc: "/assets/icon/contacts.svg", alt: "Phone Icon", width: 15, height: 15 },
   { id: 7, to: "/Catalog", title: "Catalog", imageSrc: "/assets/icon/catalog.svg", alt: "Phone Icon", width: 15, height: 15 }
 ]
@@ -13,4 +13,11 @@ export const Trophy = [
   { id: 2, src: "/assets/img/quality.png", alt: "Trophy Image", width: 100, height: 100, title: "Yüksək Səviyyəli Keyfiyyət", text: "Biz ən yaxşı istehsalçıların ən yaxşı içkilərini, şokoladlarını və spirtli içkilərini diqqətlə seçirik və yalnız ən keyfiyyətli məhsulları almağınızı təmin edirik" },
   { id: 3, src: "/assets/img/relation.png", alt: "Trophy Image", width: 100, height: 100, title: "Etibarlı Təchizatçı Münasibətləri", text: "Coca-Cola və Mars da daxil olmaqla əsas istehsalçılarla güclü əlaqələrimiz sayəsində etibarlı və etibarlı təchizatçıları təmin etmişik." },
   { id: 4, src: "/assets/img/care.png", alt: "Trophy Image", width: 100, height: 100, title: "Müstəsna Müştəri Xidmətləri", text: "Bizim xüsusi komandamız üstün müştəri xidməti göstərmək üçün yuxarıda və kənarda işləyir. Sorğuların operativ şəkildə həll edilməsindən tutmuş sifarişlərin səmərəli işlənməsini və vaxtında çatdırılmaların təmin edilməsinə qədər." },
+]
+
+export const NewsLinks = [
+  { id: 1, to: "/", title: "Biz yeni bölmə açdıq - Alkoqol!", text: "06.06.23", imageSrc: "/assets/img/newsalcohol.png", iconSrc: "/assets/icon/calendar.svg", plusSrc: "/assets/icon/plus.svg" },
+  { id: 2, to: "/", title: "Biz yeni bölmə açdıq - Alkoqol!", text: "06.06.23", imageSrc: "/assets/img/newsalcohol.png", iconSrc: "/assets/icon/calendar.svg", plusSrc: "/assets/icon/plus.svg" },
+  { id: 3, to: "/", title: "Biz yeni bölmə açdıq - Alkoqol!", text: "06.06.23", imageSrc: "/assets/img/newsalcohol.png", iconSrc: "/assets/icon/calendar.svg", plusSrc: "/assets/icon/plus.svg" },
+  { id: 4, to: "/", title: "Biz yeni bölmə açdıq - Alkoqol!", text: "06.06.23", imageSrc: "/assets/img/newsalcohol.png", iconSrc: "/assets/icon/calendar.svg", plusSrc: "/assets/icon/plus.svg" },
 ]
