e9c8bf759d2df2c7fdd9c99c4f0a92e6fb295bd1
Web-Mastermind <AZEAgent@gmail.com>
2024-02-02 19:44:17 +0400

    added about new items

--- Components/Fresh/index.tsx
+++ Components/Fresh/index.tsx
@@ -5,16 +5,20 @@ const AboutSection = () => {
   return (
     <section className={styles.aboutSection}>
       <div className="container">
-        <div className={`${styles.articleItems} ${styles.flex} ${styles.between}`}>
-          <div className={styles.articleTexts}>
-            <div className={styles.articleTextCont}>
-              <p className={`${styles.articleText} ${styles.width}`}>Müştəri məmnuniyyətinə güclü diqqət yetirməklə, biz yüksək keyfiyyətli FMCG markaları ilə əlaqəli keyfiyyət və tanınmadan ödün vermədən bizneslərə rəqabətli qiymətlərdən faydalanmağa imkan verən müstəsna dəyər təqdim etməyə çalışırıq.</p>
+        <div className={`${styles.aboutItems} ${styles.flex} ${styles.between}`}>
+          <div className={styles.aboutTexts}>
+            <div className={styles.aboutTextCont}>
+              <p className={`${styles.aboutText} ${styles.width}`}>Hazırda bizim əsas diqqətimiz FMCG (Sürətli İstehlak Malları) kateqoriyasında paralel idxal-ixrac sahəsindədir, qida məhsulları, içkilər, qəlyanaltılar və daha çox ixtisaslaşaraq, ilk növbədə topdansatış ticarəti ilə məşğul olur.</p>
+              <p className={`${styles.aboutText}`}>Özümüzdə təravət, səmərəlilik və mükəmməllik prinsiplərini təcəssüm etdiririk. Yüksək keyfiyyətli məhsul və xidmətlərin təqdim edilməsinə dair öhdəliyimiz şüarımızda əks olunub:</p>
               <h2 className={styles.aboutTitle}>Təzə. Tez. Fantastik.</h2>
             </div>
           </div>
-          <div className={styles.articleImage}>
+          <div className={styles.aboutImage}>
             <Image src="/assets/img/colasprite.png" alt="Cola Image" width={458} height={458} />
           </div>
+        </div>
+        <div className={styles.operation}>
+          <p className={styles.operationText}>Əməliyyatlarımız müxtəlif Avropa ölkələrini əhatə etməklə Litvanın hüdudlarından kənara çıxır. Biz bütün Avropadakı nüfuzlu pərakəndə satış şəbəkələri ilə güclü tərəfdaşlıq və müqavilə bağlamışıq.</p>
         </div>
       </div>
     </section>
