c0f4764dda94a04db80886f52453da937860c7b4
Web-Mastermind <AZEAgent@gmail.com>
2024-02-03 20:26:19 +0400

    nav menu chabged to side bar

--- Components/Header/index.tsx
+++ Components/Header/index.tsx
@@ -4,6 +4,7 @@ import styles from "./style.module.css"
 import Link from "next/link"
 import { useState } from "react"
 import { NavLinks } from "@/src/links/Links"
+import classNames from "classnames"
 
 const Header = () => {
   const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
@@ -20,25 +21,20 @@ const Header = () => {
             <p className={styles.listText}>Sitat əldə edin</p>
           </div>
           <Link href="/"><Image src="/assets/icon/barcode.png" alt="Barcode icon" width={156} height={60} /></Link>
-          <div className={styles.burgerIcon} onClick={toggleBurgerMenu}>
-            <Image src={!burgerMenuOpen ? "/assets/icon/menu.png" : "/assets/icon/menuclouse.svg"} alt="Menu icon" width={37} height={37} />
+          <div className={styles.burgerIcon}>
+            <input type="checkbox" role="button" aria-label="Display the menu" className={styles.menu} onClick={toggleBurgerMenu} />
           </div>
         </div>
       </div>
-      {burgerMenuOpen && (
-        <nav className={styles.nav}>
-          <div className="container">
-            <ul className={styles.burgerList}>
-              {
-                NavLinks.map(({ id, to, title, imageSrc, alt, width, height }) => {
-                  return (<Link className={` ${styles.navLinks} ${id === 1 ? styles.phone : id === 2 ? styles.mail : ""} `} href={to} key={id}>{imageSrc && <Image className={styles.linkIcons} src={imageSrc} alt={alt} width={width} height={height} />}{title}</Link>)
-                })
-              }
-            </ul>
-          </div>
-        </nav>
-      )}
-    </header>
+
+      <nav className={`${styles.nav} ${burgerMenuOpen ? styles.showMenu : ''}`}>
+        {
+          NavLinks.map(({ id, to, title, imageSrc, alt, width, height }) => {
+            return (<Link className={styles.navLinks} href={to} key={id}>{imageSrc && <Image className={styles.linkIcons} src={imageSrc} alt={alt} width={width} height={height} />}{title}</Link>)
+          })
+        }
+      </nav>
+    </header >
   )
 }
 
