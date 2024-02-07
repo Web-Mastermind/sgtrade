12e5e7785fd8d662a2effd3f2f5d596b67b93587
Web-Mastermind <AZEAgent@gmail.com>
2024-02-07 23:24:53 +0400

    fixed import css

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '@/Components/Header'
-import "./global.css"
 import Footer from '@/Components/Footer'
+import "./global.css"
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
