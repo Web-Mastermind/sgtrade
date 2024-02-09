8098652147d7a099bf36e73a813eb2162849ab28
Web-Mastermind <AZEAgent@gmail.com>
2024-02-09 23:13:01 +0400

    added new page Link

--- Components/Subscribe/index.tsx
+++ Components/Subscribe/index.tsx
@@ -11,14 +11,14 @@ const Subscribe = () => {
             <p className={styles.requestText}>Məlumat və sorğunuzu göndərin və sizinlə tezliklə əlaqə saxlayacağıq!</p>
             <div className={styles.requestLink}>
               <Image className={styles.requestImage} src="/assets/icon/requesticon.svg" alt="Send Request Icon" width={12} height={12} />
-              <Link className={`${styles.requestLinkItem} ${styles.white}`} href="/">Sorğu göndərin</Link>
+              <Link className={`${styles.requestLinkItem} ${styles.white}`} href="/Send">Sorğu göndərin</Link>
             </div>
           </div>
           <div className={`${styles.subscribeContainer} ${styles.white}`}>
             <p className={styles.subscribeText}>Yeniliklərimizə abunə olun!</p>
             <form className={styles.subscribeLink}>
               <button className={styles.subscribeButton}><Image className={styles.subscribeImage} src="/assets/icon/subicon.svg" alt="Send Request Icon" width={30} height={30} /></button>
-              <input type="email" placeholder="E-mailinizi daxil edin" />
+              <input type="email" placeholder="E-mailinizi daxil edin" required />
             </form>
           </div>
         </div>
