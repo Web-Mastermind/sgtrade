b9f3fd6c2ae07b2570bc157e9f97cf9833c9d8b0
Web-Mastermind <AZEAgent@gmail.com>
2024-02-18 22:34:00 +0400

    added new classes

--- app/(site)/ConfetCategory/[id]/page.tsx
+++ app/(site)/ConfetCategory/[id]/page.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./style.module.css"
 import Request from "@/Components/Request";
 import { prodDescription } from "@/src/Types/Interface";
+import styles from "./style.module.css"
 
 
 const url: string = process.env.NEXT_PUBLIC_CONFET || "";
@@ -16,28 +16,28 @@ const Single = async ({ params }: any) => {
       <main className={styles.singleMain}>
         <div className="container">
           <div className={styles.singleItems}>
-            <h2 className={styles.singleTitle}>{data.singleTitle}</h2>
-            <div className={styles.singleImageItems}>
+            <h2 className={`${styles.singleTitle} ${styles.white}`}>{data.singleTitle}</h2>
+            <div className={`${styles.singleImageItems} ${styles.flex}`}>
               <Image src={data.singleImage} alt="Coca Cola 0.33ml can Image" width={500} height={450} />
               <div className={styles.singleMainContainer}>
-                <div className={styles.requestContainer}>
-                  <p className={styles.singlesubText}>Anbarda</p>
-                  <div className={styles.requestLink}>
+                <div className={`${styles.requestContainer} ${styles.flex}`}>
+                  <p className={`${styles.singlesubText} ${styles.black}`}>Anbarda</p>
+                  <div className={`${styles.requestLink} ${styles.bgBlack}`}>
                     <Image className={styles.requestImage} src="/assets/icon/requesticon.svg" alt="Send Request Icon" width={12} height={12} />
                     <Link className={`${styles.requestLinkItem} ${styles.white}`} href="/Send">Sorğu göndərin</Link>
                   </div>
                 </div>
                 <p className={styles.singleFeatures}>Xüsusiyyətləri</p>
-                <div className={styles.featuresInfo}>
+                <div className={`${styles.featuresInfo} ${styles.flex} ${styles.bgBlack}`}>
                   <div className={styles.featuresInfoLeftContainer}>
                     {
                       data.prodDescription.map(({ id, icon, title, text }: prodDescription) => {
                         return (<div key={id} className={styles.map}>
-                          <div className={styles.featuresInfoItems}>
+                          <div className={`${styles.featuresInfoItems} ${styles.flex}`}>
                             <Image className={styles.featuresImageItem} src={icon} alt={title} width={16} height={18} />
                             <p className={styles.featuresTextItem}>{title}</p>
                           </div>
-                          <p className={styles.featuresMainTextItem}>{text}</p>
+                          <p className={`${styles.featuresMainTextItem} ${styles.white}`}>{text}</p>
                         </div>)
                       })
                     }
@@ -46,11 +46,11 @@ const Single = async ({ params }: any) => {
                     {
                       data.right.map(({ id, icon, title, text }: prodDescription) => {
                         return (<div key={id} className={styles.map}>
-                          <div className={styles.featuresInfoItems}>
+                          <div className={`${styles.featuresInfoItems} ${styles.flex}`}>
                             <Image className={styles.featuresImageItem} src={icon} alt={title} width={16} height={18} />
                             <p className={styles.featuresTextItem}>{title}</p>
                           </div>
-                          <p className={styles.featuresMainTextItem}>{text}</p>
+                          <p className={`${styles.featuresMainTextItem} ${styles.white}`}>{text}</p>
                         </div>)
                       })
                     }
@@ -58,21 +58,21 @@ const Single = async ({ params }: any) => {
                 </div>
               </div>
             </div>
-            <h2 className={styles.logisticTitle}>Logistika</h2>
-            <div className={styles.logisticContainer}>
+            <h2 className={`${styles.logisticTitle} ${styles.white}`}>Logistika</h2>
+            <div className={`${styles.logisticContainer} ${styles.flex}`}>
               {
                 data.bottom.map(({ id, title, text }: prodDescription) => {
                   return (<div key={id} className={styles.mapBottom}>
-                    <div className={styles.logisticItems}>
+                    <div className={`${styles.logisticItems} ${styles.bgBlack}`}>
                       <p className={styles.logisticText}>{title}</p>
-                      <p className={styles.logisticSecondText}>{text}</p>
+                      <p className={`${styles.logisticSecondText} ${styles.white}`}>{text}</p>
                     </div>
                   </div>)
                 })
               }
             </div>
-            <h3 className={styles.logisticTitle}>Məhsul</h3>
-            <p className={styles.productDesc}>{data.productInfo}</p>
+            <h3 className={`${styles.logisticTitle} ${styles.white}`}>Məhsul</h3>
+            <p className={`${styles.productDesc} ${styles.white}`}>{data.productInfo}</p>
           </div>
         </div>
       </main>
