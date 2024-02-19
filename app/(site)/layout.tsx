45d4de06c314ea485ebd89df502f277a6c2ddebc
Web-Mastermind <AZEAgent@gmail.com>
2024-02-20 00:17:37 +0400

    deleted provider

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,16 +1,15 @@
 import Footer from '@/Components/Footer'
 import Header from '@/Components/Header'
 import React from 'react'
-import Providers from './providers'
 import "./global.css"
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Providers>
+    <div>
       <Header />
       {children}
       <Footer />
-    </Providers>
+    </div>
   )
 }
 
