a768fa8fe1c682daa45239038112fb4b553fb242
Web-Mastermind <AZEAgent@gmail.com>
2024-02-22 22:32:12 +0400

    added AOS

--- Components/Products/index.tsx
+++ Components/Products/index.tsx
@@ -14,25 +14,23 @@ const Products = () => {
   }, []);
 
   return (
-    <>
-      <section className={styles.section}>
-        <div className="container">
-          <div className={styles.sectioItems}>
-            <div className={styles.sectionTitles}>
-              <h2 className={`${styles.sectioTitle} ${styles.white}`}>Məhsullar</h2>
-              <p className={`${styles.sectionText} ${styles.white}`}>Biznes ehtiyaclarınıza cavab vermək üçün sizə müxtəlif çeşidlər təklif edən, birbaşa aparıcı istehsalçılardan alınan yüksək keyfiyyətli məhsulların seçilmiş seçimini araşdırın.</p>
-            </div>
-          </div>
-          <div className={styles.sectionProducts}>
-            {
-              CategoryLinks.map(({ id, to, title, className, className2, className3 }) => {
-                return (<Link data-aos="zoom-in" key={id} className={`${styles[className]} ${styles[className2]} ${styles[className3]}`} href={to}>{title}</Link>)
-              })
-            }
+    <section className={styles.section}>
+      <div className="container">
+        <div className={styles.sectioItems}>
+          <div className={styles.sectionTitles}>
+            <h2 data-aos="fade-right" className={`${styles.sectioTitle} ${styles.white}`}>Məhsullar</h2>
+            <p data-aos="fade-left" className={`${styles.sectionText} ${styles.white}`}>Biznes ehtiyaclarınıza cavab vermək üçün sizə müxtəlif çeşidlər təklif edən, birbaşa aparıcı istehsalçılardan alınan yüksək keyfiyyətli məhsulların seçilmiş seçimini araşdırın.</p>
           </div>
         </div>
-      </section>
-    </>
+        <div className={styles.sectionProducts}>
+          {
+            CategoryLinks.map(({ id, to, title, className, className2, className3 }) => {
+              return (<Link data-aos="zoom-in" key={id} className={`${styles[className]} ${styles[className2]} ${styles[className3]}`} href={to}>{title}</Link>)
+            })
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 
